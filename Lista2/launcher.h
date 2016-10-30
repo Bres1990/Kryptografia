1999c2a16d36e0300fc378e01026610c4df6c768
Bres1990 <181710@student.pwr.edu.pl>
2016-10-31 00:44:43 +0100

    Lista 2, zadanie 1

---
+++ Lista2/launcher.h
@@ -0,0 +1,20 @@
+/*
+ * launcher.h
+ *
+ *  Created on: 30 paü 2016
+ *      Author: Adam
+ */
+
+#ifndef LAUNCHER_H_
+#define LAUNCHER_H_
+
+
+
+class launcher {
+public:
+	launcher();
+	virtual ~launcher();
+};
+
+#endif /* LAUNCHER_H_ */
+
